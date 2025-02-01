9bcc6081d1fa989b67c36d50633bac41c92f6d0f
Devershika Mohane <142586782+Devershika@users.noreply.github.com>
2025-02-01 22:42:29 +0530

    Update UserInput.java

--- UserInput.java
+++ UserInput.java
@@ -23,8 +23,21 @@ public class UserInput {
                 System.out.println("Fibonacci(" + n + ") = " + calculator.fibonacci(n));
                 break;
 
+              case 6: case 7: case 8: case 9:
+                System.out.println("Enter array size: ");
+                int size = scanner.nextInt();
+                double[] array = new double[size];
+                System.out.println("Enter elements: ");
+                for (int i = 0; i < size; i++) {
+                    array[i] = scanner.nextDouble();
+                }
+                performArrayOperation(choice, array, calculator);
+                break;
+            default:
+                System.out.println("Invalid choice!");
         }
     }
+
 
  private void performBasicOperation(int choice, double a, double b, Calculator calculator) {
         switch (choice) {
@@ -35,5 +48,15 @@ public class UserInput {
         }
     }
 
+
+private void performArrayOperation(int choice, double[] array, Calculator calculator) {
+        switch (choice) {
+            case 6: System.out.println("Sum: " + calculator.sumArray(array)); break;
+            case 7: System.out.println("Mean: " + calculator.mean(array)); break;
+            case 8: System.out.println("Variance: " + calculator.variance(array)); break;
+            case 9: System.out.println("Standard Deviation: " + calculator.standardDeviation(array)); break;
+        }
+    }
+}
 
 
